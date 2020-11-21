e1468b33053a103b62686a0c2e64fe09be1e31a2
psalmenarle <60122554+psalmenarle@users.noreply.github.com>
2020-11-21 18:18:13 +0800

    Add files via upload

---
+++ midcle/index.php
@@ -0,0 +1,86 @@
+<html>
+<head>
+<link href="http://fonts.googleapis.com/css?family=Oxygen:400,700,300" rel="stylesheet" type="text/css" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+<div id="wrapper">
+	<div id="logo" class="container">
+        <h1>Floom </h1>
+        <img  src="daisy.svg" width="80px" height="80px"/>    
+		
+	</div>
+	<div id="page" class="container">	
+		<p>The best floral arrangements are as unique and special as the people you want to send them to. Our local artisanal florists ensure that every time you order flowers online from Floom, you and your loved ones receive a loving crafted celebration of natural beauty.</p>
+	</div>
+	<div id="three-column" class="container">
+		<div class="tbox1">
+			<div class="box-style box-style01">
+				<div class="content">
+					<div class="image"><img src="flower2.svg" width="324" height="200"  /></div>
+					<h2>What you see is what you get</h2>
+					<p>Love the bouquet on your screen? That’s exactly what our local florist will prepare freshly for your order… or your money back! </p>
+					
+				</div>
+			</div>
+		</div>
+		<div class="tbox2">
+			<div class="box-style box-style02">
+				<div class="content">
+					<div class="image"><img src="flower1.svg" width="324" height="200"  /></div>
+					<h2>Always unique, never generic</h2>
+					<p>We only work with the most talented and unique artisans, and we’re passionate about supporting our skilled family of florists. </p>
+					
+				</div>
+			</div>
+		</div>
+		<div class="tbox3">
+			<div class="box-style box-style03">
+				<div class="content">
+					<div class="image"><img src="flower3.svg" width="324" height="200"  /></div>
+					<h2>Hand-delivered with care and attention</h2>
+					<p>Each of our orders is arranged, wrapped and delivered with a card on the exact day that you need it.</p>
+					
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="three-column" class="container">
+		<div class="tbox1">
+			<div class="box-style box-style01">
+				<div class="content">
+					<div class="image"><img src="flower4.svg" width="324" height="200"/></div>
+					<h2>Our inspiration comes from you</h2>
+					<p> Lana Elie founded Floom, an online marketplace that provides independent florists from the UK a platform to sell bouquets, and customers a way to buy them. </p>
+				
+				</div>
+			</div>
+		</div>
+		<div class="tbox2">
+			<div class="box-style box-style02">
+				<div class="content">
+					<div class="image"><img src="flower5.svg" width="324" height="200"  /></div>
+					<h2>Transform the way florists run their business</h2>
+					<p>We’re FloomX and we’re on a mission to completely transform the way that florists run their business' for the better. </p>
+					
+				</div>
+			</div>
+		</div>
+		<div class="tbox3">
+			<div class="box-style box-style03">
+				<div class="content">
+					<div class="image"><img src="flower6.svg" width="324" height="200"  /></div>
+					<h2>Hear what our happy customers have to say </h2>
+					<p>"Floom has been really helpful in making a couple of changes to this order. I would definitely recommend them to friends and family. Thanks very much" - Michael</p>
+					
+				</div>
+			</div>
+		</div>
+	</div>	
+</div>
+<div id="footer">
+	<p>© 2020 / FLOOM. LTD / ALL RIGHTS RESERVED</p>
+</div>
+<!-- end #footer -->
+</body>
+</html>
